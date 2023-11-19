385232a79fb622464bb7f09dfa4b2074b216e703
Gui Almeida <147108822+Guialmeiida@users.noreply.github.com>
2023-11-19 16:46:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+    let n1 = Number(prompt("Digite a 1ª nota:"))
+    let n2 = Number(prompt("Digite a 2ª nota:"))
+    let n3 = Number(prompt("Digite a 3ª nota:"))
+    const p1 = Number(5)
+    const p2 = Number(7)
+    const p3 = Number(10)
+    let media = (n1*p1 + n2*p2 + n3*p3) / (p1 + p2 + p3)
+    console.log (`A média ponderada dos valores: ${n1}, ${n2} e ${n3} é: ${media}`)
+
+    
+    
